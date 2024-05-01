b8da68762eba7a52f39854972a67ef0e923a0cb1
John Michael Delos Reyes <deltaforce8376@gmail.com>
2024-05-01 09:52:16 +0800

    HAHAH

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,50 @@
-function connect() {
-    alert("hi");
+import {FastbootDevice, FastbootError} from "../dist/js/fastboot.mjs";
+
+var device = new FastbootDevice();
+
+async function connectDevice() {
+    try {
+        await device.connect();
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    let product = await device.getVariable("product");
+    let serial = await device.getVariable("serialno");
+    let status = `Connected to ${product} (serial: ${serial})`;
+    console.log(status);
+    
+    document.getElementById("connect-button").disabled = true;
+    document.getElementById("info-button").disabled = false;
+    document.getElementById("reboot-button").disabled = false;
 }
 
-$(document).ready(() => {
-    bsCustomFileInput.init();
+async function statDevice() {
+    const cmd = await device.runCommand("oem device-info");
+    console.log(cmd);
+}
 
-    document
-        .querySelector("#connect-button")
-        .addEventListener("click", connect);
-})
+async function rebootDevice() {
+    let cmd = await device.runCommand("reboot");
+}
+
+// if the code works, dont touch it
+document
+    .getElementById("connect-button")
+    .addEventListener("click", function() {
+        console.log("connect");
+        connectDevice();
+    });
+document
+    .getElementById("info-button")
+    .addEventListener("click", function() {
+        console.log("info");
+        statDevice();
+    });
+document
+    .getElementById("reboot-button")
+    .addEventListener("click", function() {
+        console.log("reboot");
+        rebootDevice();
+    });
